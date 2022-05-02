94ff4722f9250eb79007a8b2f1953752c090d2d0
vGIT <denisvillalbarojas@gmail.com>
2022-05-02 13:27:07 -0500

    he agregado un .gitignore

---
+++ app.js
@@ -0,0 +1,15 @@
+import { createPopper } from '@popperjs/core';
+import './styles.css';
+
+const popcorn = document.querySelector('#popcorn');
+const tooltip = document.querySelector('#tooltip');
+
+createPopper(popcorn, tooltip, {
+    placement: 'top',
+    modifiers: [{
+        name: 'offset',
+        options: {
+            offset: [0, 8],
+        },
+    }, ],
+});
